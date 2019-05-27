0846434eaf1420b94508020cab3f39ef10df7a80
Irina Bright <missirinabright@gmail.com>
2019-05-27 19:14:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,25 @@
-def line(katz_deli)
-  if katz_deli.empty?
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
-  else cureent_line = "The line is currently:"
-  katz_deli.each_with_index(1) do |person, i|
-    current_line << " #{i}. #{person}"
-  end
-  puts current_line
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end 
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
 
-def now_serving(katz_deli)
-  if katz_deli.empty?
+def now_serving(deli)
+  if deli.empty?
     puts "There is nobody waiting to be served!"
-  else "Currently serving #{katz_deli.first}."
-    katz_deli.shift
+  else
+    puts "Currently serving #{deli.first}."
+    deli.shift
   end
 end
-
