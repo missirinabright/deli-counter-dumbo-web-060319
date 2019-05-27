3b91e455930fa6d690ab145db5996eea4a0161cb
Irina Bright <missirinabright@gmail.com>
2019-05-27 19:01:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,4 +2,5 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else cureent_line = "The line is currently:"
-  katz_deli.each_with_index(1) do |
+  katz_deli.each_with_index(1) do |person, i|
+    current_line << katz_deli
