4339ba8c007242f3839f1e9369a2e718dcb9ea7e
Irina Bright <missirinabright@gmail.com>
2019-05-27 19:09:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else cureent_line = "The line is currently:"
   katz_deli.each_with_index(1) do |person, i|
-    current_line << "#{i}. #{person}"
+    current_line << "#{i}. #{person}."
   end
   puts current_line
   end
